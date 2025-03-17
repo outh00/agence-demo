bff0066c24652ce3cb1cb058165a032a5a233fbe
outh00 <dima_008@hotmail.com>
2025-03-17 14:52:05 +0000

    Ajout de la démo

--- app.js
+++ app.js
@@ -1,53 +1,35 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const map = L.map('map', {
-    center: [33.5731, -7.5898],
-    zoom: 6,
-    maxBounds: [
-      [27.5, -13.5],
-      [36.5, -0.9]
-    ],
-    maxBoundsViscosity: 1.0,
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  const map = L.map('map').setView([33.5731, -7.5898], 6);
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
   }).addTo(map);
 
-  const blueIcon = new L.Icon({
+  // Définition des icônes personnalisées
+  const blueIcon = L.icon({
     iconUrl: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34]
+    iconSize: [32, 32],
+    iconAnchor: [16, 32]
   });
 
-  const redIcon = new L.Icon({
+  const redIcon = L.icon({
     iconUrl: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34]
+    iconSize: [32, 32],
+    iconAnchor: [16, 32]
   });
 
-  fetch('data_excel_based.json')
+  // Charger les données JSON et placer les marqueurs
+  fetch('data.json')
     .then(response => response.json())
     .then(data => {
+      // Afficher les communes en bleu
       data.communes.forEach(commune => {
-        L.marker([commune.latitude, commune.longitude], {icon: blueIcon})
-          .bindPopup(`
-            <strong>Commune :</strong> ${commune.name}<br>
-            <strong>Région :</strong> ${commune.region}<br>
-            <strong>Province/Préfecture :</strong> ${commune.province_prefecture}<br>
-            <strong>Besoin Province CCT VL :</strong> ${commune.BesoinProvinceCCT_VL}<br>
-            <strong>Besoin Province CCT PL :</strong> ${commune.BesoinProvinceCCT_PL}<br>
-            <strong>Besoin Commune CCT VL :</strong> ${commune.BesoinCommuneCCT_VL}<br>
-            <strong>Besoin Commune CCT PL :</strong> ${commune.BesoinCommuneCCT_PL}<br>
-            <strong>Taux Remplissage Commune VL :</strong> ${commune.TauxRemplissageCommuneVL}<br>
-            <strong>Taux Remplissage Commune PL :</strong> ${commune.TauxRemplissageCommunePL}<br>
-            <strong>Nombre CVT :</strong> ${commune.Nombre_CVT}<br>
-            <strong>Nombre CVT PL :</strong> ${commune.Nombre_CVT_PL}
-          `)
+        L.marker([commune.latitude, commune.longitude], { icon: blueIcon })
+          .bindPopup(`<b>Commune:</b> ${commune.name}`)
           .addTo(map);
       });
 
+      // Afficher les centres en rouge
       data.centres.forEach(centre => {
         const [lat, lng] = centre.GPS.split(',').map(Number);
         if (!isNaN(lat) && !isNaN(lng)) {
@@ -62,6 +44,6 @@ document.addEventListener("DOMContentLoaded", function () {
             .addTo(map);
         }
       });
-    })
-    .catch(err => console.error("Erreur lors du chargement des données :", err));
+  })
+  .catch(err => console.error("Erreur lors du chargement des données :", err));
 });
