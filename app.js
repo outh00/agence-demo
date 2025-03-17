03d92156ad74f053c9b94f6fa32bfa39d8b9d23c
outh00 <dima_008@hotmail.com>
2025-03-17 22:16:47 +0000

    Message décrivant les modifications

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Initialisation de la carte
-var map = L.map('map').setView([32.3683, -6.3692], 6); // Centre du Maroc
+var map = L.map('map').setView([32.3683, -6.3692], 7); // Centre de la région Béni Mellal-Khénifra
 
 // Ajout des tuiles OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -10,6 +10,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 const regionColor = "#FF5733"; // Orange pour les régions
 const provinceColor = "#33FF57"; // Vert pour les provinces
 const communeColor = "blue"; // Bleu pour les communes
+const centreColor = "red"; // Rouge pour les centres
 
 // Charger les données depuis le fichier JSON
 fetch('data.json')
@@ -20,7 +21,7 @@ fetch('data.json')
         // Affichage des régions comme zones
         data.regions.forEach(region => {
             if (region.polygon) {
-                let polygon = L.polygon(region.polygon, { color: regionColor, fillOpacity: 0.3 })
+                L.polygon(region.polygon, { color: regionColor, fillOpacity: 0.3 })
                     .addTo(map)
                     .bindPopup(`<b>Région:</b> ${region.nom}`);
             }
@@ -29,24 +30,43 @@ fetch('data.json')
         // Affichage des provinces comme zones plus petites
         data.provinces.forEach(province => {
             if (province.polygon) {
-                let polygon = L.polygon(province.polygon, { color: provinceColor, fillOpacity: 0.3 })
+                L.polygon(province.polygon, { color: provinceColor, fillOpacity: 0.4 })
                     .addTo(map)
                     .bindPopup(`<b>Province:</b> ${province.nom}`);
             }
         });
 
-        // Affichage des communes comme points
+        // Affichage des communes comme points à l'intérieur des provinces
         data.communes.forEach(commune => {
             if (commune.latitude && commune.longitude) {
                 L.circleMarker([commune.latitude, commune.longitude], {
                     color: communeColor,
-                    radius: 5
+                    radius: 6,
+                    fillOpacity: 0.8
                 }).bindPopup(`
                     <b>Commune:</b> ${commune.nom}<br>
                     <b>Besoins VL:</b> ${commune.BesoinCommuneCCT_VL}<br>
                     <b>Besoins PL:</b> ${commune.BesoinCommuneCCT_PL}<br>
                     <b>Population:</b> ${commune.population}
                 `).addTo(map);
+
+                // Ajouter les centres (max 2 par commune)
+                let centres = commune.centres || [];
+                centres.slice(0, 2).forEach(centre => {
+                    L.marker([commune.latitude, commune.longitude], {
+                        icon: L.icon({
+                            iconUrl: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
+                            iconSize: [32, 32],
+                            iconAnchor: [16, 32]
+                        })
+                    }).bindPopup(`
+                        <b>Centre:</b> ${centre.nom}<br>
+                        <b>Adresse:</b> ${centre.adresse}<br>
+                        <b>Horaires:</b> ${centre.horaires}<br>
+                        <b>Téléphone:</b> ${centre.telephone}<br>
+                        <b>Statut:</b> ${centre.statut}
+                    `).addTo(map);
+                });
             }
         });
     })
