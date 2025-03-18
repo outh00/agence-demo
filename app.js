94119aead710f17e91986ce3bf0117a514d824a7
outh00 <dima_008@hotmail.com>
2025-03-18 02:07:06 +0000

    Message décrivant les modifications

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Initialisation de la carte avec un zoom de départ sur Béni Mellal-Khénifra
+// Initialisation de la carte centrée sur Béni Mellal-Khénifra
 var map = L.map('map').setView([32.5, -6.5], 7);
 
 // Ajout des tuiles OpenStreetMap
@@ -6,11 +6,19 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Définition des groupes de calques pour gérer le zoning
+// Déclaration des groupes de calques
 var layerRegions = L.layerGroup().addTo(map);
 var layerProvinces = L.layerGroup();
 var layerCommunes = L.layerGroup();
 var layerCentres = L.layerGroup();
+
+// Définition des couleurs pour chaque niveau
+var colors = {
+    region: 'yellow',
+    province: 'red',
+    commune: 'blue',
+    centre: 'purple'
+};
 
 // Chargement des données depuis `data.json`
 fetch('data.json')
@@ -18,44 +26,42 @@ fetch('data.json')
     .then(data => {
         console.log("Données chargées :", data);
 
-        // --- 🔶 Affichage des Régions (Zoom 6-7) ---
+        // 🟡 Ajout des Régions
         data.regions.forEach(region => {
             L.polygon(region.polygon, {
-                color: 'yellow',
-                fillColor: '#FFFF00',
+                color: colors.region,
+                fillColor: colors.region,
                 fillOpacity: 0.3
             }).bindPopup(`<b>Région :</b> ${region.nom}`).addTo(layerRegions);
         });
 
-        // --- 🔴 Affichage des Provinces (Zoom 8-9) ---
+        // 🔴 Ajout des Provinces
         data.provinces.forEach(province => {
-            var provinceLayer = L.polygon(province.polygon, {
-                color: 'red',
-                fillColor: '#FF0000',
+            L.polygon(province.polygon, {
+                color: colors.province,
+                fillColor: colors.province,
                 fillOpacity: 0.3
-            }).bindPopup(`<b>Province :</b> ${province.nom}`);
-            layerProvinces.addLayer(provinceLayer);
+            }).bindPopup(`<b>Province :</b> ${province.nom}`).addTo(layerProvinces);
         });
 
-        // --- 🔵 Affichage des Communes (Zoom 10-11) ---
+        // 🔵 Ajout des Communes
         data.communes.forEach(commune => {
-            var communeLayer = L.polygon(commune.polygon, {
-                color: 'blue',
-                fillColor: '#0000FF',
+            L.polygon(commune.polygon, {
+                color: colors.commune,
+                fillColor: colors.commune,
                 fillOpacity: 0.3
             }).bindPopup(`
                 <b>Commune :</b> ${commune.nom}<br>
                 <b>Population :</b> ${commune.population}<br>
                 <b>Besoin VL :</b> ${commune.BesoinCommuneCCT_VL}<br>
                 <b>Besoin PL :</b> ${commune.BesoinCommuneCCT_PL}
-            `);
-            layerCommunes.addLayer(communeLayer);
+            `).addTo(layerCommunes);
         });
 
-        // --- 🟣 Affichage des Centres (Zoom 12+) ---
+        // 🟣 Ajout des Centres (points)
         data.communes.forEach(commune => {
             commune.centres.forEach(centre => {
-                var centreMarker = L.marker([centre.latitude, centre.longitude], {
+                L.marker([centre.latitude, centre.longitude], {
                     icon: L.icon({
                         iconUrl: 'https://maps.google.com/mapfiles/ms/icons/purple-dot.png',
                         iconSize: [32, 32],
@@ -67,29 +73,30 @@ fetch('data.json')
                     <strong>Horaires :</strong> ${centre.horaires}<br>
                     <strong>Téléphone :</strong> ${centre.telephone}<br>
                     <strong>Statut :</strong> ${centre.statut}
-                `);
-                layerCentres.addLayer(centreMarker);
+                `).addTo(layerCentres);
             });
         });
 
-        // Gestion du zoom progressif
+        // Gestion du zoom progressif sans effacer les données
         map.on('zoomend', function() {
             var zoomLevel = map.getZoom();
             console.log("Niveau de zoom actuel :", zoomLevel);
 
-            if (zoomLevel < 8) {  // Région seulement
-                map.addLayer(layerRegions);
+            // Toujours afficher la région
+            map.addLayer(layerRegions);
+
+            if (zoomLevel < 8) {  
                 map.removeLayer(layerProvinces);
                 map.removeLayer(layerCommunes);
                 map.removeLayer(layerCentres);
-            } else if (zoomLevel >= 8 && zoomLevel < 10) { // Provinces visibles
+            } else if (zoomLevel >= 8 && zoomLevel < 10) { 
                 map.addLayer(layerProvinces);
                 map.removeLayer(layerCommunes);
                 map.removeLayer(layerCentres);
-            } else if (zoomLevel >= 10 && zoomLevel < 12) { // Communes visibles
+            } else if (zoomLevel >= 10 && zoomLevel < 12) { 
                 map.addLayer(layerCommunes);
                 map.removeLayer(layerCentres);
-            } else { // Centres visibles
+            } else { 
                 map.addLayer(layerCentres);
             }
         });
